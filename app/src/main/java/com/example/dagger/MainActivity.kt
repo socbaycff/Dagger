aff94329d0c9f729af3a47fd04b4fad49f16715b
socbaycff <socbaycff@gmail.com>
2020-02-14 12:18:42 +0700

    update binding

--- app/src/main/java/com/example/dagger/MainActivity.kt
+++ app/src/main/java/com/example/dagger/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.dagger
 
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.example.dagger.databinding.ActivityMainBinding
 import com.example.dagger.temp.ViewModelProviderFactory
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
@@ -14,14 +16,16 @@ class MainActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
 
         val viewModel = ViewModelProvider(
             this,
             providerFactory as ViewModelProvider.Factory
         ).get(MainViewModel::class.java)
         viewModel.setup()
-
-        Log.i("testttt",viewModel.data.value)
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = this
+        Log.i("testttt",viewModel.data.value as String)
     }
 }
